84e8a4ee67e0adb984fcbe6d86423a4cac7cc850
Juan David <gottygarcia@gmail.com>
2021-02-18 01:04:57 -0500

    react-icons and rest operator

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
+import { categories } from '../../api/db.json'
 
 export const ListOfCategories = () => {
-  return (
-    <List>
-      {
-        [1, 2, 3, 4].map(category => <Item key={category}><Category /></Item>)
-      }
-    </List>
-  )
+	return (
+		<List>
+			{categories.map(category => (
+				<Item key={category.id}>
+					<Category {...category} />
+				</Item>
+			))}
+		</List>
+	)
 }
