2817d4b47c8728c412bb96d2ff54331a8b8a65f7
Juan David García <gottygarcia@gmail.com>
2021-03-01 22:18:01 -0500

    curso terminado

--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { FavsWithQuery } from '../container/GetFavorites'
 import { Layout } from '../components/Layout'
 
-export const Favs = () => (
+const Favs = () => (
 	<Layout title="Tus favoritos" subtitle="Aquí puedes encontrar tus favoritos">
 		<h1>Favs</h1>
 		<FavsWithQuery />
 	</Layout>
 )
+
+export default Favs
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { ListOfCategories } from '../components/ListOfCategories'
 import { ListOfPhotoCards } from '../container/ListOfPhotoCards'
 import { Layout } from '../components/Layout'
 
-export const Home = ({ categoryId }) => {
+export const HomePage = ({ categoryId }) => {
 	return (
 		<Layout title="Tu app de fotos de mascotas" subtitle="fotos de animales">
 			<ListOfCategories />
@@ -11,3 +11,7 @@ export const Home = ({ categoryId }) => {
 		</Layout>
 	)
 }
+
+export const Home = React.memo(HomePage, (prevProps, props) => {
+	return prevProps.categoryId === props.categoryId
+})
